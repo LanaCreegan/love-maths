8f76c3a5cf044973cdc3e6d7126fcf446195e03c
LanaCreegan <Lanacreegan17@gmail.com>
2023-01-29 13:41:32 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,7 +52,10 @@ function runGame(gameType) {
    } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
 
-   } else {
+   }  else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2); 
+   
+  } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
    }
@@ -97,7 +100,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"]; 
- } else {
+    }  else if (operator === "/") {
+        return [operand1 / operand2, "division"]; 
+
+  } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -144,6 +150,15 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+    
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
     
 }
